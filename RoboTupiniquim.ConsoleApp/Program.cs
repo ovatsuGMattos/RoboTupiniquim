c27538f2157530bd1435a190ab367fd23235a643
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-04-01 03:05:02 -0300

    Add project files.

---
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoboTupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
